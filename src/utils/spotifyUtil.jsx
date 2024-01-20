8f0565f208cddf95a1168ca30dc57bf68c362b9d
Connor Baltich <45185643+CanadianBleach@users.noreply.github.com>
2024-01-19 19:32:42 -0500

    Update spotifyUtil.jsx

--- src/utils/spotifyUtil.jsx
+++ src/utils/spotifyUtil.jsx
@@ -52,7 +52,7 @@ function getProfile() {
 
 async function fetchPlaylists() {
   const response = await fetch(
-    "https://api.spotify.com/v1/me/playlists?limit=100",
+    "https://api.spotify.com/v1/me/playlists?limit=50",
     {
       method: "GET",
       headers: { Authorization: "Bearer " + currentToken.access_token },
